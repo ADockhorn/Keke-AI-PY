afd7dda0a7fe627bf89c3f9c8e54fd2aa7e3b4b5
Robert Jesse Ronge <robert.jesse.ronge@gmail.com>
2025-02-11 16:24:28 +0100

    limit test time

--- Keke_PY/experiments/soo_linear.sh
+++ Keke_PY/experiments/soo_linear.sh
@@ -8,7 +8,7 @@
 #SBATCH --array=0-0                   # Es werden 5 Tasks mit den IDs von 0-4 gestartet
 #SBATCH --cpus-per-task=20
 #SBATCH --mem-per-cpu=4G
-#SBATCH --time=12:00:00
+#SBATCH --time=02:00:00
 #SBATCH --output=soo_all_levels_%A_%a-out.txt   # Logdatei für den merged STDOUT/STDERR output (%A wird durch slurm Job-ID ersetzt und %a durch den Array Index)
 
 
